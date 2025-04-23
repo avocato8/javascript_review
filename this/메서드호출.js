995360d05e8dd9047809cc24e7c6894257fcc447
avocato8 <nightfury805@gmail.com>
2025-04-23 20:44:28 +0900

    250423임시

---
+++ this/메서드호출.js
@@ -0,0 +1,10 @@
+//매서드 내부의 this에는 매서드를 호출한 객체가 바인딩
+const person = {
+  name: "Lee",
+  getName() {
+    //메서드 내부의 this는 메서드를 호출한 객체에 바인딩
+    return this.name;
+  },
+};
+
+console.log(person.getName()); // Lee
