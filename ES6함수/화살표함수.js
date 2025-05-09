6e4874c2d21a6969bb0a10255ea0587c0ac50078
avocato <nightfury805@gmail.com>
2025-05-09 22:42:42 +0900

    250509-임시

--- ES6함수/화살표함수.js
+++ ES6함수/화살표함수.js
@@ -31,3 +31,30 @@ const person = (name) =>
 });
 //ES6
 [1, 2, 3].map((v) => v * 2); // [2, 4, 6]
+
+//화살표 함수와 일반 함수의 차이
+//1. 화살표 함수는 인스턴스를 생성할 수 없는 non-constructor이다.
+const Foo = () => {};
+Foo.hasOnwProperty("prototype"); // false
+
+//2. 중복된 매개변수 이름을 선언할 수 없다.
+//일반 함수는 중복된 매개변수 이름을 선언해도 에러가 발생하지 않는다.
+//화살표 함수에서 중복된 매겨변수 이름을 선언하면 에러가 발생한다.
+
+//3. 화살표 함수는 함수 자체의 this, arguments, super, new.target 바인딩을 갖지 않는다.
+//따라서 화살표 함수 내부에서 this, arguments, super, new.target을 참조하면 스코프 체인을 통해
+//상위 스코프의 this, arguments, super, new.target을 참조한다.
+
+//this
+//화살표 함수가 일반 함수와 구별되는 가장 큰 특징은 this이다.
+//this는 함수를 호출할 때 함수가 어떻게 호출되었는지에 따라 this에 바인딩할 객체가 동적으로 결정된다.
+class Prefixer {
+  constructor(prefix) {
+    this.prefix = prefix;
+  }
+  add(arr) {
+    return arr.map(function (item) {
+      return this.prefix + item;
+    });
+  }
+}
