a2233d93ccb47cbf9b92e1bc51030f4ae8282b6e
avocato8 <nightfury805@gmail.com>
2025-06-10 23:43:40 +0900

    250610

--- 배열/배열메서드.js
+++ 배열/배열메서드.js
@@ -60,7 +60,6 @@ if (!foods1.includes("orange")) {
 }
 console.log(foods1); //['apple', 'banana', 'orange']
 
-
 //Array.prototype.push
 //push 메서드는 인수로 전달받은 모든 값을 왼본 배열의 마지막 요소로 추가하고 변경된 length 프로퍼티 값을 반환
 const arr2 = [1, 2];
@@ -81,3 +80,148 @@ const newArr = [...arr2, 6];
 consolelog(newArr); // [1, 2, 3, 4, 5, 6]
 
 //Array.prototype.pop
+//pop메서드는 원본 배열에서 마지막 요소를 제거하고 제거한 요소를 반환한다.
+//원본 배열이 빈 배열이면 undefined를 반환한다.
+const arr3 = [1, 2];
+let result3 = arr3.pop();
+console.log(result3); // 2
+
+//pop 메서드는 원본 배열을 직접 변경
+console.log(arr3); // [1]
+
+//pop메서드와 push 메서드를 사용하면 스택을 쉽게 구현할 수 있다.
+//스택을 생성자 함수로 구현하면,
+const Stack = (function () {
+  function Stack(array = []) {
+    if (!Array.isArray(array)) {
+      throw new TypeError(`${array} is not an array.`);
+    }
+    this.array = array;
+  }
+  Stack.prototype = {
+    constructor: Stack,
+    push(value) {
+      return this.array.push(value);
+    },
+    pop() {
+      return this.array.pop();
+    },
+    entries() {
+      return { ...this.array };
+    },
+  };
+  return Stack;
+})();
+
+const stack = new Stack([1, 2]);
+console.log(stack.entries()); // [1, 2]
+
+stack.push(3);
+console.log(stack.entries()); // [1, 2, 3]
+
+stack.pop();
+console.log(stack.entries()); // [1, 2]
+
+//스택을 클래스로 구현하면,
+class Stack1 {
+  #array; //private class member
+
+  constructor(array = []) {
+    if (!Array.isArray(array)) {
+      throw new TypeError(`${array} is not an array`);
+    }
+    this.#array = array;
+  }
+
+  push(value) {
+    return this.#array.push(value);
+  }
+  pop() {
+    return this.#array.pop();
+  }
+  entries() {
+    return [...this.#array];
+  }
+}
+
+const stack1 = new Stack1([1, 2]);
+console.log(stack1.entries()); //[1, 2]
+
+stack1.push(3);
+console.log(stack.entries()); // [1, 2, 3]
+
+//Array.prototype.unshift 메서드
+const arr4 = [1, 2];
+
+//인수로 전달받은 모든 값을 원본 배열의 선두에 요소로 추가하고, 변경된 length값을 반환한다.
+let result4 = arr4.unshift(3, 4);
+console.log(result4); // 4
+//unshift 메서드는 원본 배열을 직접 변경한다.
+console.log(arr4); // [3, 4, 1, 2]
+//따라서 스프레드 문법 ...을 되도록 사용하자!
+const arr5 = [1, 2];
+const newArr5 = [3, ...arr];
+console.log(newArr5); // [3 ,1, 2]
+
+//Array.prototype.shift
+//shift 메서드는 원본 배열에서 첫 번째 요소를 제거하고 제거한 요소를 반환한다.
+//원본 배열이 빈 배열이면 undefined를 반환한다.
+const arr6 = [1, 2];
+let result6 = arr6.shift();
+console.log(result6); // 1
+//원본 배열을 직접 변경한다.
+console.log(arr6); // [2]
+
+//shift와 pop을 사용하여 큐 구현
+const Queue = (function () {
+  function Queue(array = []) {
+    if (!Array.isArray(array)) {
+      throw new TypeError(`${array} is not an array.`);
+    }
+    this.array = array;
+  }
+  Queue.prototype = {
+    constructor: Queue,
+    enqueue(value) {
+      return this.array.push(value);
+    },
+    dequeue() {
+      return this.array.shift();
+    },
+    entries() {
+      return [...this.array];
+    },
+  };
+  return Queue;
+})();
+
+//클래스로 구현
+class Queue1 {
+  #array;
+
+  constructor(array = []) {
+    if (!Array.isArray(array)) {
+      throw new TypeError(`${array} is not an array`);
+    }
+  }
+
+  enqueue(value) {
+    return this.#array.push(value);
+  }
+  dequeue() {
+    return this.#array.shift();
+  }
+  entries() {
+    return [...this.#array];
+  }
+}
+const queue = new Queue([1, 2]);
+console.log(queue.entries()); // [1, 2]
+
+queue.enqueue(3);
+console.log(queue.entries()); // [1, 2, 3]
+
+queue.dequeue();
+console.log(queue.entries()); // [2, 3]
+
+//Array.prototype.splice
